df2bdc8ed44f956fd0871b6fdbb710137a4e108d
Simon Jacobs <sdjacobs@uchicago.edu>
2014-12-29 10:11:58 -0800

    minimal working prototype

--- www/app/main.js
+++ www/app/main.js
@@ -1,15 +1,100 @@
 define(function (require) {
-    // Load any app-specific modules
-    // with a relative require call,
-    // like:
-    var messages = require('./messages');
-
-    // Load library/vendor modules using
-    // full IDs, like:
-    var print = require('print');
 
     var d3 = require('d3');
-    d3.select("#gobaby").on("click", function() { alert("hello")})
+    
+    var interact = require('interact');
+   
+    var colors = d3.scale.category10();
+    function dialog(node) { 
 
-    print(messages.getHello());
+
+        var obj = {
+            // call this function on every dragmove event
+            onmove: function (event) {
+                var target = event.target,
+                    // keep the dragged position in the data-x/data-y attributes
+                    x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx,
+                    y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy;
+
+                // translate the element
+                target.style.webkitTransform =
+                target.style.transform =
+                    'translate(' + x + 'px, ' + y + 'px)';
+
+                // update the posiion attributes
+                target.setAttribute('data-x', x);
+                target.setAttribute('data-y', y);
+            }
+        };
+
+        // target elements with the "draggable" class
+        var d = interact('.draggable')
+            .draggable(obj)
+            .resizable(true)
+            .on('resizemove', function (event) {
+                var target = event.target;
+
+                // add the change in coords to the previous width of the target element
+                var
+                  newWidth  = parseFloat(target.style.width ) + event.dx,
+                  newHeight = parseFloat(target.style.height) + event.dy;
+
+                // update the element's style
+                target.style.width  = newWidth + 'px';
+                target.style.height = newHeight + 'px';
+            });
+
+
+        d3.select(node).style("background-color", colors(colors.domain().length));
+        
+        var y = document.querySelectorAll('select, input, button, .container')
+        for (var i = 0; i < y.length; i++){
+            y[i].onmouseover = function() { d.draggable(false) }
+            y[i].onmouseout = function() { d.draggable(true) }
+        }
+    }
+   
+    dialog('.draggable')
+
+    d3.json("modules.json", function(modules) {
+        d3.select("#menu").selectAll("li")
+           .data(modules).enter()
+            .append("li")
+            .append("a")
+            .attr("href", "#")
+            .text(function(d) { return d.name })
+            .on("click", display)
+            .append("br")
+        
+    });
+
+
+    function display(module) {
+        
+        d3.xhr(module.page, "text/html", function(req) {
+            var div = document.createElement("div")
+            div.innerHTML = req.response;
+            
+            div.setAttribute("class", "draggable");
+            
+            div.setAttribute("style", "width: 300px; height: 200px;")
+            document.body.appendChild(div);
+            dialog(div)
+            
+            var viz = d3.select("body")
+                        .append("div")
+                        .attr("class", "draggable")
+                        .attr("style", "width:500px;height:500px")
+            
+            var v = viz
+                        .append("div")
+                        .attr("class", "container")
+                        .attr("style", "width:100%; height:100%;")
+                        .style("background-color", 'white')
+                        
+            dialog(viz.node());
+
+            require(module.deps, function(f) { f(v.node()); });
+        });
+    }
 });
---
+++ www/lib/graphCanvas.js
@@ -0,0 +1,213 @@
+
+define(['d3'], function (d3) {
+    function graphCanvas() {
+    
+
+    var nodes = undefined,
+        edges = undefined,
+        context = undefined,
+        canvas = undefined,
+        selectedNodes = [],
+        force = undefined,
+        labels = false;
+   
+    var width = 0, height = 0; // these change in draw()
+
+    var zoom = d3.behavior.zoom();
+
+    var colors = (function() {
+        var i = 0;
+        var col = ["red", "orange", "yellow", "green", "brown", "purple"]
+        function nextColor() {
+            var c = col[i]
+            i = (i + 1) % col.length
+            return c;
+        }
+        return nextColor
+    })()
+
+
+    var graph = function(view) {
+        width = parseInt(view.style("width")), height = parseInt(view.style("height"));
+        
+        canvas = view
+            .append("canvas")
+            .attr("width", width)
+            .attr("height", height)
+            .attr("pointer-events", "all")
+            .call(zoom)
+            .node()
+
+        context =  canvas.getContext("2d")
+
+        if (!force)
+            force = d3.layout.force()
+                .gravity(.05)
+                .distance(100)
+                .charge(-100)
+                .size([width, height]);
+
+        return graph;
+    }
+
+    graph.start = function() {
+        force
+            .nodes(nodes)
+            .links(edges)
+            .on("end", function() { zoom.on("zoom", redraw) })
+            .on("start", function() { zoom.on("zoom", undefined) })
+            .start()
+        window.requestAnimationFrame(redraw)
+        return graph;
+    }
+
+    graph.stop = function() {
+        force.alpha(0);
+        return graph;
+    };
+
+    function redraw() {
+        redraw.started = true; /* other things can redraw now */
+
+        context.clearRect(0,0,canvas.width,canvas.height)
+        
+        var s = zoom.scale(),
+            t = zoom.translate()
+
+        /* Batch path drawing */
+        context.beginPath();
+        context.strokeStyle = 'lightgrey';
+        edges.forEach(function(d) {
+            context.moveTo(s * d.source.x + t[0], s * d.source.y + t[1])
+            context.lineTo(s * d.target.x + t[0], s * d.target.y + t[1])
+        });
+        context.stroke()
+
+        nodes.forEach(function(d) {
+            context.beginPath();
+            context.arc(s * d.x + t[0], s * d.y + t[1], s * (d.size || 8), 0, 2 * Math.PI);
+            context.fillStyle = d.color ||  "black";
+            context.fill();
+        });
+
+        if (labels) {
+            context.font = (s * 10).toString() + "pt Arial"
+            context.fillStyle = "black";
+            nodes.forEach(function(d) {
+                context.fillText(d.label, s * (d.x + 8) + t[0], s * d.y + t[1])
+            });
+        } 
+        
+        if (force.alpha())
+            window.requestAnimationFrame(redraw)
+    }
+
+    graph.center = function() {
+        zoom.scale(0.25);
+        zoom.translate([width/4,height/4])
+        redraw()
+    }
+
+    graph.selectNode = function(node) {
+        
+        selectedNodes.push(node)
+        node.oldcolor = node.color;
+        node.color = d3.hsl(colors());
+
+        if (!node.neighbors)
+            node.neighbors = findNeighbors(nodes, edges, node)
+
+        node.neighbors.forEach(function(d) {
+            d.oldcolor = d.color
+            d.color = node.color.brighter(1.2)
+        })
+      
+        zoom.scale(1)
+        zoom.translate([width/2 - node.x, height/2 - node.y])
+        
+        if (! force.alpha())
+            redraw();
+    }
+
+    graph.unSelectNode = function(node) {
+        node.color = node.oldcolor;
+        node.neighbors.forEach(function(d) {
+            d.color = d.oldcolor;
+        });
+        selectedNodes.remove(node);
+        if (!force.alpha())
+            redraw();
+    }
+
+    graph.nodes = function(_) {
+        if (!arguments.length)
+            return nodes;
+        else
+            nodes = _;
+        return graph;
+    }
+
+    graph.edges = function(_) {
+        if (!arguments.length)
+            return edges;
+        else
+            edges = _;
+        return graph;
+    }
+
+
+    graph.selectedNodes = function(_) {
+        if (!arguments.length)
+            return selectedNodes;
+        else
+            selectedNodes = _;
+        return graph;
+    }
+
+    graph.labels = function(_) {
+        if (!arguments.length)
+            return labels;
+        else
+            labels = _;
+        if (redraw.started)
+            redraw()
+        return graph;
+    }
+
+    graph.redraw = function() {
+        if (!force)
+            return
+        if (force.alpha())
+            return
+        redraw()
+    }
+
+    graph.force = function(_) {
+        if (!arguments.length)
+            return force;
+        else
+            force = _;
+        return graph;
+    }
+
+
+    
+    function cleanName(name) {
+        if (+name > 0)
+            return "_" + name;
+        else
+            return name.replace("'", "_apos_");
+    }
+
+    function selectNode(name) {
+        return d3.select("#" + cleanName(name));
+    }
+
+
+
+    return graph;
+
+}
+
+return graphCanvas;
+});
